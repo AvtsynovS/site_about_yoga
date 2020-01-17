ea4a64c519b3a41c0a6103040b3b1fe3efffedd4
Sergey Avtsynov <Avtsynov.S@yandex.ru>
2020-01-18 00:52:49 +0300

    site about yoga

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+/* Показать/скрыть часть статьи. Работает только через Id. */
+/*
+function more () {
+  let dots = document.getElementById ("dots");
+  let more = document.getElementById ("more");
+  let btn = document.getElementById ("buttonMore");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btn.innerHTML = "Подробнее";
+      more.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btn.innerHTML = "Скрыть";
+      more.style.display = "inline";
+    }
+}
+*/
